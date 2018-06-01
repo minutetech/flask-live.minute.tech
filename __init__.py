8695cc8ec7fc3fbab93149a5e0bdbccd54e3574e
Douglas James <douglasrcjames@gmail.com>
2018-06-01 13:30:45 -0700

    mobile optimizing

--- __init__.py
+++ __init__.py
@@ -1611,4 +1611,4 @@ def file_downloads():
 ############################################ END ACCOUNT SYSTEM #########################################################
 
 if __name__ == "__main__":
-	app.run(debug=False)
+	app.run(debug=True)
